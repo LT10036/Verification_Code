825bdc3324d892e37b3947801c7c7a92480d0fd3
lt10036 <lt10036@sina.com>
2019-06-20 11:18:46 +0800

    图片验证码识别，暂时只能识别数字

---
+++ 图片识别_01.py
@@ -0,0 +1,29 @@
+
+#-------------------------------------------------------------------------------------------------------------------------
+#
+# 修 改  pytesseract  文 件 中 的 tesseract_cmd 地 址 为：c://Program Files (x86)//Tesseract-OCR//tesseract.exe
+# 初 级 版 本 识 别 数字 验 证 码
+# 发 现 0 o 不 分 ，9 g 不 分 ，不 分 清 楚 能 个 别 数 字 和 字 母
+#
+# ------------------------------------------------------------------------------------------------------------------------
+
+
+from PIL import Image
+import pytesseract
+
+
+# 设置语言包目录 必须加上这句，暂时没解决根本起因
+tessdata_dir_config = '--tessdata-dir "c://Program Files (x86)//Tesseract-OCR//tessdata"'
+
+# 空白列表接收单个字符
+a=[]
+
+# 打开图片，并解析
+l=Image.open('D://Documents//Downloads//123.bmp')
+text = pytesseract.image_to_string(l,lang='eng',config=tessdata_dir_config)
+
+# 解析出来的文本有空格字符，需要解析下
+for i in text:
+    if i !=" ":
+        a.append(i)
+print(a)
